04948f0e4533f45db95f3bbf41515a54ada3270d
Ana Martinez <analeticia.martinez@yahoo.com.br>
2024-04-15 13:44:31 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -3,7 +3,10 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
@@ -11,7 +14,8 @@ public class Category implements Serializable {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
+@Transient
+    private Set<Product> products = new HashSet<>();
     public Category(){
 
     }
@@ -35,6 +39,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+    @Transient
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
